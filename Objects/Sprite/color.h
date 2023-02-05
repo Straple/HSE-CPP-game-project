635494978a23aa2ec162fb819ce39d26e1f9bbba
straple <54230867+Straple@users.noreply.github.com>
2023-02-05 21:41:49 +0300

    Update color.h

--- Objects/Sprite/color.h
+++ Objects/Sprite/color.h
@@ -7,25 +7,30 @@
 // [a, r, g, b]
 // [0..255]
 struct Color {
-    u8 b = 0;  // blue
-    u8 g = 0;  // green
-    u8 r = 0;  // red
-    u8 a = 0;  // alpha
+    u8 b;  // blue
+    u8 g;  // green
+    u8 r;  // red
+    u8 a;  // alpha
 
-#define calc_color(alpha, c) ((static_cast<unsigned int>(alpha) * c) / 255)
+#define calc_color(alpha, c) ((static_cast<unsigned int>(alpha) * (c)) / 255)
 
-    Color() = default;
+    Color() {
+        b = g = r = a = 0;
+    }
 
-    Color(u8 red, u8 green, u8 blue, u8 alpha = 0xff)
+    Color(u8 red, u8 green, u8 blue, u8 alpha)
         : a(alpha),
           r(calc_color(alpha, red)),
           g(calc_color(alpha, green)),
           b(calc_color(alpha, blue)) {
     }
 
+    Color(u8 red, u8 green, u8 blue) : a(0xff), r(red), g(green), b(blue) {
+    }
+
     // 0xRRGGBB, alpha
-    Color(unsigned int rgb, u8 alpha) {
-        *reinterpret_cast<unsigned int *>(this) = rgb;
+    Color(unsigned int rgb, u8 alpha)
+        : b(rgb & 0xff), g((rgb & 0xff00) >> 8), r((rgb & 0xff0000) >> 16) {
         r = calc_color(alpha, r);
         g = calc_color(alpha, g);
         b = calc_color(alpha, b);
@@ -33,8 +38,11 @@ struct Color {
     }
 
     // 0xAARRGGBB
-    Color(unsigned int argb) {
-        *reinterpret_cast<unsigned int *>(this) = argb;
+    Color(unsigned int argb)
+        : b(argb & 0xff),
+          g((argb & 0xff00) >> 8),
+          r((argb & 0xff0000) >> 16),
+          a((argb & 0xff000000) >> 24) {
         r = calc_color(a, r);
         g = calc_color(a, g);
         b = calc_color(a, b);
@@ -42,14 +50,12 @@ struct Color {
 
     // накладывает на этот цвет color
     [[nodiscard]] Color combine(const Color &color) const {
-        u8 inv_alpha = 0xff - color.a;
-
         Color result;
-        result.a = 0xff - calc_color(inv_alpha, 0xff - a);
+        u8 inv_alpha = 0xff - color.a;
         result.r = color.r + calc_color(inv_alpha, r);
         result.g = color.g + calc_color(inv_alpha, g);
         result.b = color.b + calc_color(inv_alpha, b);
-
+        result.a = color.a + calc_color(inv_alpha, a);
         return result;
     }
 
@@ -61,7 +67,6 @@ struct Color {
 };
 
 bool operator==(const Color &lhs, const Color &rhs) {
-    // very fast
     return lhs.b == rhs.b && lhs.g == rhs.g && lhs.r == rhs.r && lhs.a == rhs.a;
 }
 
