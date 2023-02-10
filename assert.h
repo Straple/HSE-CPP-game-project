7ee89a72c24b12bdf35eedf5056ca405f14505c3
straple <54230867+Straple@users.noreply.github.com>
2023-02-10 23:16:44 +0300

    made ASSERT more attractive

--- assert.h
+++ assert.h
@@ -1,14 +1,17 @@
-﻿#ifndef GAME_ENGINE_ASSERT
+#ifndef GAME_ENGINE_ASSERT
 #define GAME_ENGINE_ASSERT
 
 #include <iostream>
 
+#define GAME_ENGINE_DEBUG_MODE  // use debug mode
+
 #ifdef GAME_ENGINE_DEBUG_MODE
 
-#define ASSERT(condition, message)         \
-    if (!(condition)) {                    \
-        std::cerr << message << std::endl; \
-        std::exit(0);                      \
+#define ASSERT(condition, message)                                            \
+    if (!(condition)) {                                                       \
+        std::cerr << "assert failed at " __FILE__ << ":" << __LINE__ << '\n'; \
+        std::cerr << "message: \"" << message << "\"\n";                      \
+        std::exit(0);                                                         \
     }
 
 //_STL_VERIFY(condition, message) // don't work on CLion
