72034ee9befc434401d63655adf0a511b8c8acc5
Егор Юхневич <hadayegor@gmail.com>
2023-05-21 23:37:51 +0300

    fix Table

--- Game/Game objects/table.hpp
+++ Game/Game objects/table.hpp
@@ -9,20 +9,20 @@ struct Table : abstract_game_object {
 
     explicit Table(const Dot new_pos) {
         size = 1;
-        delta_draw_pos = Dot(-10, 15);
+        delta_draw_pos = Dot(-10, 30);
         collision_radius = 10;
         pos = new_pos;
     }
 
     [[nodiscard]] std::unique_ptr<Collision> get_collision() const override {
-        return std::make_unique<CollisionBox>(pos + delta_draw_pos + Dot(0, -5), pos - delta_draw_pos);
+        return std::make_unique<CollisionBox>(pos + delta_draw_pos + Dot(0, -5), pos + Dot(-delta_draw_pos.x, 0));
     }
 
     void draw() const override {
         // draw shadow
         {
             Dot shadow_p0 = pos + delta_draw_pos + Dot(-1, 0);
-            Dot shadow_p1 = pos - delta_draw_pos + Dot(1, -10);
+            Dot shadow_p1 = pos + Dot(-delta_draw_pos.x, 0) + Dot(1, -10);
             static_pos_update(shadow_p0, global_variables::camera_is_static);
             static_pos_update(shadow_p1, global_variables::camera_is_static);
             draw_rect2(shadow_p0, shadow_p1, Color(0, 128));
